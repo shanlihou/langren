ad79f3e29c6486eeb051b13ad41757446fe7fe4c
shanlihou <shanlihou@gmail.com>
2016-11-30 07:11:29 -0800

    send

--- langren/view.py
+++ langren/view.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from xml.dom.minidom import parse
 import xml.dom.minidom
 import hashlib
+import time
 def langren(request):
 	return HttpResponse("Hello langren ! ")
 
@@ -45,7 +46,9 @@ def root(request):
 		ret = "<xml>"	
 		ret = ret + "<ToUserName><![CDATA[" + dictReq["openid"] + "]]></ToUserName>"
 		ret = ret + "<FromUserName><![CDATA[geyeguojiang]]></FromUserName>"
-		ret = ret + "<CreateTime>12345678</CreateTime>"
+		strTime = str(int(time.time()))
+		print strTime
+		ret = ret + "<CreateTime>" + strTime + "</CreateTime>"
 		ret = ret + "<MsgType><![CDATA[text]]></MsgType>"
 		ret = ret + "<Content><![CDATA[hello]]></Content>"
 		ret = ret + "</xml>"
