b5840fea3b64ff9c1f144cb06554dae1dfa9df9b
shanlihou <shanlihou@gmail.com>
2017-03-28 00:26:36 +0800

    could control

--- control.py
+++ control.py
@@ -1,5 +1,18 @@
+#!/usr/bin/python
 import urllib2
+import time
+import os
 def getUrl(url):
 	resp = urllib2.urlopen(url)
 	return resp.read()
-print getUrl('http://192.168.0.189:8000/control?isNeedClose=1')	
+def main():
+	while(1):
+		time.sleep(5)
+		getUrl('http://60.205.206.18:80/control?pcOpen=1')
+		status = getUrl('http://60.205.206.18:80/control?isNeedClose=1')
+		print 'status:' + status
+		if status == '0':
+			getUrl('http://60.205.206.18:80/control?pcClose=1')
+			os.system('shutdown -s -t 300')
+			time.sleep(305)
+main()
